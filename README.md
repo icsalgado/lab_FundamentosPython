c50f9e658959214f2808791068ee52ad361946e5
icsalgado <icsalgado@icloud.com>
2023-01-27 05:30:34 -0300

    [v1.0]DIO-Fundamentos de Python - Lab sistema bancario - ajustes na aparencia e tratamento de erros e entrada invalida

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # lab_FundmentosPython
+
+referências
+https://dicasdepython.com.br/python-como-limpar-a-tela-do-console/
+
+https://medium.com/cwi-software/como-definir-um-timeout-para-uma-fun%C3%A7%C3%A3o-em-python-22f74e3aa20c
--- lab_caixa_eletronico.py
+++ lab_caixa_eletronico.py
@@ -1,11 +1,18 @@
+import os #biblioteca para ajudar a limpar a tela
+import time #biblioteca para implementar um timeout, só funciona em unix
+
+#41
+cabecalho_menu = """
+    ===== SISTEMA BANCARIO GENERICO =====
+"""
+
 menu = """
+    [1] Deposito                Saque [2]
+    [3] Extrato                  Sair [4]
 
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[q] Sair
+    =====================================
 
-=> """
+Teclado => """
 
 saldo = 0
 limite = 500
@@ -15,44 +22,117 @@ LIMITE_SAQUES = 3
 
 #print(menu)
 
-valor_zerado = "Deposito minimo de R$ 1.00"
+erro = " OPERACAO NAO CONCLUIDA "
+fecha_bloco = ""
+sucesso = " OPERACAO CONCLUIDA COM SUCESSO "
 
 while True:
-
+    os.system('clear') or None #Isso limpa a tela
+    print(cabecalho_menu)
+    print((f"Saldo: R$ {saldo:.2f}").center(41, " "))
     opcao = input(menu)
 
-    if opcao == "d":
-        valor = float(input("Informe o valor do deposito: "))
+    if opcao == "1":
+        os.system('clear') or None
+        print((" DEPOSITO ").center(41, "=") + "\n")
+
+        try:
+            valor = float(input("valor do deposito:\n=> "))
+        except:
+            os.system('clear') or None
+            print(erro.center(41, "="))
+            print("\n" + ("Entrada invalida para deposito de valores").center(41, " ") + "\n")
+            print(fecha_bloco.center(41, "="))
+            time.sleep(3)
+            continue
 
         if valor > 0:
             saldo += valor
-            extrato += f"Deposito: R$ {valor:.2f}\n"
+            extrato += (f"\n [+] Deposito: R$ {valor:.2f}\n" + (f"Saldo na data: R$ {saldo:.2f}").rjust(41, " "))
+            os.system('clear') or None
+            print(sucesso.center(41, "="))
+            print("\n" + (f"R$ {valor} depositado").rjust(41, " ") + "\n")
+            print(fecha_bloco.center(41, "="))
+            time.sleep(3)
         else:
-            print(valor_zerado)
-    elif opcao == "s":
-        valor = float(input("Informe o valor do saque: "))
+            os.system('clear') or None
+            print(erro.center(41, "="))
+            print("\n" + ("Deposito minimo R$ 1.00").center(41, " ") + "\n")
+            print(fecha_bloco.center(41, "="))
+            time.sleep(3) #trava a execução em segundos no unix - OBS windows não deve funcionar
+    elif opcao == "2":
+        os.system('clear') or None
+        print((" SAQUE ").center(41, "=") + "\n")
+        print((f"Saldo: R$ {saldo:.2f}").rjust(41, " "))
+        
+        try:
+            valor = float(input("valor do saque:\n=> "))
+        except:
+            os.system('clear') or None
+            print(erro.center(41, "="))
+            print("\n" + ("Entrada invalida para saque de valores").center(41, " ") + "\n")
+            print(fecha_bloco.center(41, "="))
+            time.sleep(3)
+            continue
 
         excedeu_saldo = valor > saldo
         excedeu_limite =  valor > limite
         excedeu_saques = numero_saques >= LIMITE_SAQUES
 
         if excedeu_saldo:
-            print("Saldo insuficiete")
+            os.system('clear') or None
+            print(erro.center(41, "="))
+            print("\n" + ("Saldo Insuficiete!").center(41, " "))
+            print((f"Valor Disponivel R$: {saldo:.2f}").center(41, " ") + "\n")
+            print(fecha_bloco.center(41, "="))
+            time.sleep(3)
         elif excedeu_limite:
-            print("Valor acima do limite permitido")
+            os.system('clear') or None
+            print(erro.center(41, "="))
+            print("\n" + ("Saque acima do limite permitido!").center(41, " "))
+            print((f"Limite atual R$: {limite:.2f}").center(41, " ") + "\n")
+            print(fecha_bloco.center(41, "="))
+            time.sleep(3)
         elif excedeu_saques:
-            print("Quantidade de saques excedida")
+            os.system('clear') or None
+            print(erro.center(41, "="))
+            print("\n" + ("Limite de quantidade de saques excedido!").center(41, " "))
+            print((f"Limite atual de saques: {numero_saques}").center(41, " ") + "\n")
+            print(fecha_bloco.center(41, "="))
+            time.sleep(3)
         elif valor > 0:
             saldo -= valor
-            extrato += f"Saque: R$ {valor:.2f}\n"
+            extrato += (f"\n [-] Saque: R$ {valor:.2f}\n" + (f"Saldo na data: R$ {saldo:.2f}").rjust(41, " "))
             numero_saques += 1
+            os.system('clear') or None
+            print(sucesso.center(41, "="))
+            print("\n" + (f"R$ {valor:.2f} sacado").rjust(41, " ") + "\n")
+            print(fecha_bloco.center(41, "="))
+            time.sleep(3)
         else:
-            print(valor_zerado)
-    elif opcao == "e":
-        print("Extrato")
-        print("Sem moviemntacoes" if not extrato else extrato)
-        print(f"Saldo: R$ {saldo:.2f}\n")
-    elif opcao == "q":
+            os.system('clear') or None
+            print(erro.center(41, "="))
+            print("\n" + ("Valor invalido para saque").center(41, " ") + "\n")
+            print(fecha_bloco.center(41, "="))
+            time.sleep(3)
+    elif opcao == "3":
+        os.system('clear') or None
+        print((" EXTRATO ").center(41, "="))
+        print((f"Saldo: R$ {saldo:.2f}").rjust(41, " "))
+        print("\nSem movimentacoes\n" if not extrato else extrato)
+        print(fecha_bloco.center(41, "="))
+        time.sleep(5)
+    elif opcao == "4":
+        os.system('clear') or None
+        print(cabecalho_menu)
+        print((" OBRIGADO PELA PREFERENCIA, ATE LOGO ").rjust(41, " "))
+        print("\n    " + (fecha_bloco.center(37, "=")))
+        time.sleep(3)
+        os.system('clear') or None
         break
     else:
-        print("Opcao invalida")
+        os.system('clear') or None
+        print(erro.center(41, "="))
+        print("\n" + ("Digite apenas os numeros do menu").center(41, " ") + "\n")
+        print(fecha_bloco.center(41, "="))
+        time.sleep(3)
